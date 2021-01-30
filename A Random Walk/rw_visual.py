0f4f6890c08d5f72430957b2c4b706996cb0286e
Endvr13 <endvrtrade@gmail.com>
2021-01-30 18:13:25 +0000

    Random walk "Try it yourself" 15-3 through 15-5

--- A Random Walk/rw_visual.py
+++ A Random Walk/rw_visual.py
@@ -5,7 +5,7 @@ from random_walk import RandomWalk
 # Keep making new walks as long as the program is active
 while True:
     # Make a random walk
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(5_000)
     rw.fill_walk()
 
     # Plot the points in the walk
@@ -13,10 +13,10 @@ while True:
     fig, ax = plt.subplots(figsize=(20, 12), dpi=82)
     point_numbers = range(rw.num_points)
 
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.plasma, edgecolors='none', s=1)
+    ax.plot(rw.x_values, rw.y_values, c='red', lw=1)
     # Emphasise first and last points
-    ax.scatter(0, 0, c='midnightblue', edgecolors='black', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='gold', edgecolors='black', s=100)
+    ax.plot(0, 0)
+    ax.plot(rw.x_values[-1], rw.y_values[-1])
 
     # Remove axes
 
